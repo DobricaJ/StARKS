f4e7f2e9dca6bcef4f03bbb0cda53ee08d1fb5c5
Dobrica Jadzic <dobrica.jadzic@gmail.com>
2021-05-12 15:17:35 +0200

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using StARKS.Enums;
+using System;
+
+namespace StARKS.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public Gender Gender { get; set; }
+    }
+}
