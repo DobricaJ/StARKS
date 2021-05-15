8a4555e05bc7f302b68ef93d4647ab9dbd6925c2
Dobrica Jadzic <dobrica.jadzic@gmail.com>
2021-05-15 22:15:35 +0200

    Fixed validation

--- Models/Course.cs
+++ Models/Course.cs
@@ -1,11 +1,14 @@
-﻿namespace StARKS.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StARKS.Models
 {
     public class Course
     {
         public int Id { get; set; }
-        
-        public string Code { get; set; }
 
+        [Required]
+        public string Code { get; set; }
+        [Required]
         public string Name { get; set; }
 
         public string Description { get; set; }
--- Models/Student.cs
+++ Models/Student.cs
@@ -8,8 +8,10 @@ namespace StARKS.Models
     {
         public int Id { get; set; }
 
+        [Required]
         public string FirstName { get; set; }
 
+        [Required]
         public string LastName { get; set; }
 
         public string Address { get; set; }
